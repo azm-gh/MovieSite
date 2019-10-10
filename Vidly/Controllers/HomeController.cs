7399c1467de1fc5e63c808c8c7eec5b506afcc70
azm19 <jimmy.j@centrum.cz>
2019-10-10 09:55:47 +0200

    first commit

---
+++ Vidly/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("Customers")]
+        public ActionResult Customers()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "John Cleese", Id=1},
+                new Customer {Name = "Cindy Mayes", Id=2}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Customers/Details/{customerId}")]
+        public ActionResult CustomerDetail(int customerId)
+        {
+            //ViewBag.Message = "Here goes the customer detail";
+            return Content(customerId + "");
+        }
+
+        [Route("Movies")]
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "John Cleese"},
+                new Customer {Name = "Cindy Mayes"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model;
+            //return View(movie);
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page =1, sortBy = "name1" });
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:range(1,12):regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        
+    }
+}  
